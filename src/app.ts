1380c897c0517f17373b2bb3991b2163e875f660
itsjahid01 <jahidnoman0@gmail.com>
2024-05-26 01:41:03 +0600

    db connected

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World jahid !");
+  res.send("Hello World!");
 });
 
 export default app;
