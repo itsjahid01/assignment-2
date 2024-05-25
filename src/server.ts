802e90e2ee0239c2fe23a9cb3a706cec81b44273
itsjahid01 <jahidnoman0@gmail.com>
2024-05-25 20:41:07 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const port = 3000;
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+main();
