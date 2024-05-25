58d7004429e4a77a9386e737bc24eb1061279369
itsjahid01 <jahidnoman0@gmail.com>
2024-05-26 03:36:59 +0600

    db connected

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { productRoutes } from "./modules/product/product.route";
 const app = express();
 
 // parser
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/products", productRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
---
+++ src/modules/product/product.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+const createProduct = (req: Request, res: Response) => {};
+
+export const productController = {
+  createProduct,
+};
